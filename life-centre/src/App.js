ec47bbf0adeb68465d134a657e430fe80a00b2d6
MattyStrem <mathew.k.street@gmail.com>
2021-05-24 07:59:23 +0100

    first commit

---
+++ life-centre/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "./features/header/header"
+
+//import logo from './logo.svg';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="background-container"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ life-centre/src/features/header/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "../../css/header.css"
+
+const Header = () => {
+
+
+return (
+    <header>
+        <div className="logo">
+            <h3>Life Centre</h3>
+        </div>
+    </header>
+    );
+
+}
+
+export default Header;
