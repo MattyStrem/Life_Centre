4e89ad1a15b45cf99f497beb42a4e96c228b0bc7
Mathew Street <mathew.k.street@gmail.com>
2021-05-28 17:11:37 +0100

    unsplash added

--- life-centre/src/api/unsplash.js
+++ life-centre/src/api/unsplash.js
@@ -1,2 +1,15 @@
 const API_URL = `https://api.unsplash.com/search/photos?query=nature&page=1&per_page=5&client_id=${configUnsplashAPI.ACCESS_KEY}`;
 
+export const getImage = async () => {
+    const response = await fetch(API_URL);
+    const json = await response.json();
+
+    return json.results.map((result) => result.urls.full);
+};
+
+const unsplashApi = {
+    getImage
+};
+
+export default unsplashApi;
+
--- life-centre/src/store/backgroundImageSlice.js
+++ life-centre/src/store/backgroundImageSlice.js
@@ -1,5 +1,7 @@
 import { createSlice, createSelector } from '@reduxjs/toolkit';
 
+import unsplashApi from '../api/unsplash';
+
 const initialState = {
     backgroundImages: ['https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
                   'https://images.unsplash.com/photo-1506501139174-099022df5260?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
@@ -10,12 +12,15 @@ const initialState = {
 
 const backgroundImageSlice = createSlice({
     name: 'backgroundImage',
-    initialState: initialState,
+    initialState: {
+        imageUrls: [],
+        currentImageUrlIndex: 0,
+    },
     reducers: {
 
     }
 });
 
-export const selectBackgroundImage = (state) => state.backgroundImage;
+export const selectBackgroundImage = (state) => state.imageUrls[currentImageUrlIndex];
 
 export default backgroundImageSlice.reducer;
